5b8faf14f85978c6a4df23a5c094e98b78a43d94
jinho <jinho74350721@gmail.com>
2022-10-14 22:58:35 +0900

    언리얼 C++ 프로젝트 추가

---
+++ ShootingCpp/Source/ShootingCpp/Private/CodingTest.cpp
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodingTest.h"
+
+// Sets default values
+ACodingTest::ACodingTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	
+	UE_LOG(LogTemp, Warning, L"Contructor");
+}
+
+// Called when the game starts or when spawned
+void ACodingTest::BeginPlay()
+{
+	Super::BeginPlay();
+	// 한글
+	// 한글
+	UE_LOG(LogTemp, Warning, TEXT("string test %d\n"), Add(number1, number2));
+}
+
+// Called every frame
+void ACodingTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+int32 ACodingTest::Add(int32 num1, int32 num2)
+{
+	return num1 + num2;
+}
+
---
+++ ShootingCpp/Source/ShootingCpp/Public/CodingTest.h
@@ -0,0 +1,47 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CodingTest.generated.h"
+
+UCLASS()
+class SHOOTINGCPP_API ACodingTest : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnyWhere, BlueprintReadWrite)
+	int32 number1 = 10;
+
+	UPROPERTY(EditAnyWhere, BlueprintReadOnly)
+	int32 number2 = 20;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 EditDefaultOnly = 20;
+	
+	UPROPERTY(EditInstanceOnly)
+	int32 EditInstanceOnly = 20;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString unrealString = TEXT("한글 테스트");
+	
+public:	
+	// Sets default values for this actor's properties
+	ACodingTest();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	int32 Add(int32 num1, int32 num2);
+
+
+};
